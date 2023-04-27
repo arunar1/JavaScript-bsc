fe9e4042d9b0b69a3b4d012e5d8f1d6c88db2460
arunar1 <arunarofficial00@gmail.com>
2023-04-27 19:20:09 +0530

    addedMobileMenu

--- bsc/live/js/live.js
+++ bsc/live/js/live.js
@@ -1,6 +1,8 @@
 const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
+    effect: 'cube',
+    speed:5000,
     loop: true,
   
     // If we need pagination
@@ -8,14 +10,44 @@ const swiper = new Swiper('.swiper', {
       el: '.swiper-pagination',
     },
   
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+    // // Navigation arrows
+    // navigation: {
+    //   nextEl: '.swiper-button-next',
+    //   prevEl: '.swiper-button-prev',
+    // },
   
     // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+    // autoplay:{
+    //     delay:1000,
+    // }
   });
+
+//   var swiper = new Swiper(".Swiper", {
+//     spaceBetween: 30,
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//     pagination: {
+//             el: '.swiper-pagination',
+//           },
+// autoplay:{
+//   delay:1000,
+// }
+//   });
+
+//mobile menu
+
+let mobileMenu=document.querySelector("header .fa-bars");
+// console.log(mobileMenu);
+
+let menu=document.querySelector(".menu");
+
+mobileMenu.addEventListener('click',openMenu);
+
+function openMenu(){
+  menu.classList.toggle('open');
+}
