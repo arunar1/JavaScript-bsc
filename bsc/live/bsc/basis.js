5af73e0fb32db6dfef28659f76330afaac793f33
arunar1 <arunarofficial00@gmail.com>
2023-04-30 20:10:19 +0530

    commitBasis

---
+++ bsc/live/bsc/basis.js
@@ -0,0 +1,34 @@
+// let isRainingToday=true;
+
+// let isMatchToday;
+
+
+// if(isRainingToday)
+// {
+//     isMatchToday="There is no match";
+// }
+// else{
+//     isMatchToday="match today";
+// }
+
+// console.log(isMatchToday);
+
+let playerFirstName='';
+let playerMidName='chunni';
+let playerLastName='kohli';
+
+let tshirtDisplayName=playerFirstName||playerMidName||playerLastName;
+
+// console.log(tshirtDisplayName);
+
+
+// let advertisers=true;
+// let audience=true;
+
+// let finelDesicion=advertisers&&audience;
+
+// console.log(finelDesicion?"math today":"no match today");
+
+
+//nullish ??
+console.log(tshirtDisplayName);
